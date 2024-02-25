ac277b105e9d3659db700fff7488bc75e1074ec8
Doose4 <157078178+Doose4@users.noreply.github.com>
2024-02-25 10:33:49 -0600

    Update Code

--- Meet-in-the-Middle/Main.cpp
+++ Meet-in-the-Middle/Main.cpp
@@ -7,86 +7,133 @@
 
 using namespace std;
 
-int* read()
+vector<int> read(vector<int> row, string filename)
 {
     ifstream fin;
     string line;
     string val;
-    int* row_ptr;
-    vector<int> row;
     
 
     // Open an existing file
-    fin.open("Data1.csv");
+    fin.open(filename);
     fin >> line;
     cout << line << endl;
 
-
+    // Set's values in a vector with commas being the devider
     stringstream s(line);
     while (getline(s, val, ',')) {
         row.push_back(stoi(val));
     }
 
-    for (auto& val : row) {
-        cout << val << "  ";
-    }
+    //for (auto& val : row) {
+    //    cout << val << "  ";
+    //}
+
     fin.close();
 
-    row_ptr = &row[0];
-
-    return row_ptr;
+    return row;
 }
 
-int main()
-{
+float even_medium(vector<int> row) {
+    int size = row.size();
+    size = ((size) / 2);
+    float val1 = row[size];
+    float val2 = row[size - 1];
+    float med;
+    med = (val1 + val2) / 2;
+    return med;
+
+}
+
+float odd_medium(vector<int> row) {
+    int size = row.size();
+    size = ((size - 1) / 2);
+    return row[size];
+}
+
+float find_medium(vector<int> row) {
+    float med;
+
+    int size = row.size();
+
+    if ((size % 2) == 0) {
+        med = even_medium(row);
+    }
+    else {
+        med = odd_medium(row);
+    }
+
+    return med;
+}
+
+void assert(float val1, float val2){
+    if (val1 == val2) { 
+        cout << "Test Good!" << endl;
+    }
+    else {
+        cout << "Test Failed! :(" << endl;
+        cout << "The actual value was : " << val1 << endl
+            << "This does not match: " << val2 << endl;
+    }
+}
+
+void tester(vector<int> row) {
+    float med;
+    //tests all the data sets
+
+    row = read(row, "Data1.csv");
+    med = find_medium(row);
+    assert(med, 25);
+    row.clear();
+
+    row = read(row, "Data2.csv");
+    med = find_medium(row);
+    assert(med, 53);
+    row.clear();
+
+    row = read(row, "Data3.csv");
+    med = find_medium(row);
+    assert(med, 5.5);
+    row.clear();
+
+    row = read(row, "Data4.csv");
+    med = find_medium(row);
+    assert(med, 10);
+    row.clear();
+
+    row = read(row, "Data5.csv");
+    med = find_medium(row);
+    assert(med, 10);
+    row.clear();
+
+    row = read(row, "Data6.csv");
+    med = find_medium(row);
+    assert(med, 15.5);
+    row.clear();
+
+    cout << "Test completed!" << endl;
+}
+
+int main() {
     vector<int> row;
-    int* row_pnt;
-    row_pnt = read();
-    cout << row_pnt;
+    string userinput; 
+    float med;
+
+    cout << "Please enter the file name: " << endl;
+    cin >> userinput;
+
+    if (userinput == "test") 
+    {
+        tester(row);
+        return 0;
+    }
+
+    row = read(row, userinput);
+
+    med = find_medium(row);
+    
+
+    cout << "The medium number is: " << med << endl;
+
     return 0;
 }
-
-//#include <iostream>
-//#include <fstream>
-//#include <sstream>
-//
-//#include <string>
-//#include <vector>
-//
-//using namespace std;
-//
-//int main(int argc, char** argv) {
-//
-//    string line;                    /* string to hold each line */
-//    vector<vector<int>> array;      /* vector of vector<int> for 2d array */
-//
-//    if (argc < 2) { /* validate at least 1 argument given */
-//        cerr << "error: insufficient input.\n"
-//            "usage: " << argv[0] << " filename\n";
-//        return 1;
-//    }
-//
-//    ifstream f(argv[1]);   /* open file */
-//    if (!f.is_open()) {     /* validate file open for reading */
-//        perror(("error while opening file " + string(argv[1])).c_str());
-//        return 1;
-//    }
-//
-//    while (getline(f, line)) {         /* read each line */
-//        string val;                     /* string to hold value */
-//        vector<int> row;                /* vector for row of values */
-//        stringstream s(line);          /* stringstream to parse csv */
-//        while (getline(s, val, ','))   /* for each value */
-//            row.push_back(stoi(val));  /* convert to int, add to row */
-//        array.push_back(row);          /* add row to array */
-//    }
-//    f.close();
-//
-//    cout << "complete array\n\n";
-//    for (auto& row : array) {           /* iterate over rows */
-//        for (auto& val : row)           /* iterate over vals */
-//            cout << val << "  ";        /* output value      */
-//        cout << "\n";                   /* tidy up with '\n' */
-//    }
-//    return 0;
-//}
